1d6e65ecb84bf7f9fbc6d4557accad5753fcb98f
jllanquev <jllanquev@unjbg.edu.pe>
2025-07-10 22:47:52 -0500

    ACTUALIZACION; Se le agrego la opcion de agregar candidatos

--- ONPE.cpp
+++ ONPE.cpp
@@ -18,12 +18,13 @@ struct Datos{
 };
 
 void LeerCorreo(Correo &, string, string);
-void LeerUser(Datos &,string,string,string,int,Correo,int,string);
+void LeerUser(Datos &,string,string,string,int,int,Correo,string);
 
 int main(){
 	int Opcion,edad,dni,n=0;
-	string sexo,partido,nombre,correo,dom,lema;
+	string sexo,partido,nombre,user,dom,lema;
 	Datos User[200], cont;
+	Correo Email;
 	cout<<"*****************************************************"<<endl;
 	cout<<"                          ONPE                      "<<endl;
 	cout<<"*****************************************************"<<endl;
@@ -43,13 +44,22 @@ int main(){
 			cout<<"***********************************************************"<<endl;
 			cout<<"Ingrese los datos:  "<<endl;
 			cout<<"Nombre: "; cin>>nombre;
-			cout<<"Sexo: "; cin>>sexo;
-			cout<<"PartidoPo";cin>>partido;
+			cout<<"Sexo (M|F): "; cin>>sexo;
+			cout<<"PartidoPo: ";cin>>partido;
 			cout<<"Edad: ";cin>>edad;
-			cout<<"Usuario del correo: "; cin>>correo;
-			cout<<"Dominio del correo: "; cin>>dom;
-			cout<<"Dni: ";
-			cout<<"Lema: "<<endl;
+			cout<<"Dni: "; cin>>dni;
+			cout<<"Lema: "; cin>>lema;
+			cout<<"Ingrese el correo electronico (Usuario@dominio): "<<endl;
+			cout<<"\tUsuario del correo: "; cin>>user;
+			cout<<"\tDominio del correo: "; cin>>dom;
+
+			LeerCorreo(Email,user,dom);
+			LeerUser(cont,nombre,sexo,partido,edad,dni,Email,lema);
+
+			User[n] = cont;
+			n++;
+			system ("pause");
+			break;
 			
 		case 2:
 		case 3:
@@ -58,4 +68,22 @@ int main(){
 		case 6:
 			break;
 	}
+	return 0;
 }
+
+void LeerCorreo(Correo &a, string b,string c){
+	a.User=b;
+	a.Domain=c;
+}
+void LeerUser(Datos &a,string b,string c,string d,int e,int f,Correo g,string h){
+	a.Nombre=b;
+	a.Sexo=c;
+	a.PartidoPo=d;
+	a.Edad=e;
+	a.Dni=f;
+	a.Email=g;
+	a.Lema=h;
+	
+}
+	
+
